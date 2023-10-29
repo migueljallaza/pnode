914c1ea4ac4e867d3f6c3f9234dcb90ea14c288f
migueljallaza <38589552+migueljallaza@users.noreply.github.com>
2023-10-29 00:08:58 -0400

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,7 @@ export const sequelize = new Sequelize(
   'postgres', // username
   '5GCg6Ec5bGf3**bb6dG5DafCF5cG-CG6', // password
   {
-    host: 'viaduct.proxy.rlwy.net',
+    host: 'viaduct.proxy.rlwy.net:26587',
     dialect: 'postgres',
   }
 );
